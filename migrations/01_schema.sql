1feb702f586ec441b5ea59f8aba654a46cb5a22b
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-13 18:02:13 -0400

    It will have details about the properties

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -10,3 +10,23 @@ CREATE TABLE users (
   password VARCHAR(255)
 );
 
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active BOOLEAN
+);
+
+
